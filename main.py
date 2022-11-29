046c72b38aa969840f59c69581e79b701ba22489
jose99II <87508552+jose99II@users.noreply.github.com>
2022-11-29 04:12:37 -0600

    pdfs

--- main.py
+++ main.py
@@ -3,7 +3,60 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView
 from PyQt5.QtCore import  QPropertyAnimation,QEasingCurve
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.uic import loadUi
+     
+from fpdf import FPDF
+        
+from referencias import *
 from conexion import Comunicacion
+
+
+
+
+
+class PDF(FPDF):
+        
+            def header(self):
+                self.image('logo.png',
+                        x = 10, y = 10, w = 30, h = 30)
+        
+                self.set_font('Arial', '', 15)
+        
+                tcol_set(self, 'blue')
+                tfont_size(self,45)
+                tfont(self,'B')
+                self.cell(w = 0, h = 20, txt = 'Nota', border = 0, ln=1,
+                        align = 'C', fill = 0)
+        
+                tfont_size(self,10)
+                tcol_set(self, 'black')
+                tfont(self,'I')
+                self.cell(w = 0, h = 10, txt = 'Generado el 2020/11/20', border = 0, ln=2,#FECHA AUQI VA AQUI
+                        align = 'C', fill = 0)
+        
+                self.ln(5)
+        
+            # Page footer
+            def footer(self):
+                # Position at 1.5 cm from bottom
+                self.set_y(-20)
+        
+                # Arial italic 8
+                self.set_font('Arial', 'I', 12)
+        
+                # Page number
+                self.cell(w = 0, h = 10, txt =  'Pagina ' + str(self.page_no()) + '/{nb}',
+                         border = 0,
+                        align = 'C', fill = 0)   
+        
+        
+        
+        
+        
+        
+
+
+
+
 
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
@@ -468,6 +521,7 @@ class VentanaPrincipal(QMainWindow):
         self.a.setText(str(jo))
         self.base_datos.meteractualizacon(var[0],self.total,iva,jo,self.NUMFACTURA[0])
         self.base_datos.bajar(self.NUMFACTURA[0])
+        self.crearPdf()
 
     def Limpia(self):
         self.act_buscar.clear()
@@ -479,27 +533,155 @@ class VentanaPrincipal(QMainWindow):
         self.Apellido.clear()
         self.Materno.clear()
         
+    def crearPdf(self):
+        lista_datos = (
+        (1, 'Carlos', 'carlos@gmail.com', '2020-02-25'),
+        (2, 'Jose', 'jose@gmail.com', '2019-03-12'),
+        (3, 'Marcos', 'marcos@gmail.com', '2018-01-31'),
+        (4, 'Luz', 'luz@gmail.com', '2017-02-15'),
+        (5, 'Elmer', 'elmer@gmail.com', '2016-11-23'),
+        (6, 'Elmer', 'elmer@gmail.com', '2016-11-23'),
+        )#*8
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-               
-
-
-
-
+        pdf = PDF(orientation = 'P', unit = 'mm', format='A4') 
+        pdf.alias_nb_pages()
+        
+        pdf.add_page()
+        
+        # TEXTO
+        pdf.set_font('Arial', '', 15) 
+        
+        
+        # 1er encabezado ----
+        
+        bcol_set(pdf, 'green')
+        tfont_size(pdf,15)
+        tfont(pdf,'B')
+        pdf.multi_cell(w = 0, h = 15, txt = 'REFACCIONARIA CORONEL', border = 0,
+                 align = 'C', fill = 1)
+        tfont(pdf,'')
+        
+        
+        h_sep = 15
+        pdf.ln(3)
+        tfont_size(pdf,12)
+        
+        # fila 1 --
+        
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = 'Nombre:', border = 0, 
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black')         
+        pdf.cell(w = 45, h = h_sep, txt = 'AlexS', border = 0,#SE ESCRIBE NOMBRE AQUI
+                 align = 'L', fill = 0)
+        
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = '#CLIENTE:', border = 0, 
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black') 
+        pdf.multi_cell(w = 0, h = h_sep, txt = 'alex7320k@gmail.com', border = 0,#AQUI VA OTRO DATO NUMERO DE CLIENTE
+                 align = 'L', fill = 0)
+        
+        
+        # fila 2 --
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = 'Apellido:', border = 0, 
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black')
+        pdf.cell(w = 45, h = h_sep, txt = 'ChuquillanquiSS', border = 0,#VA UN APELLIDO O LOS DOS AQUI
+                 align = 'L', fill = 0)
+        
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = 'Fecha:', border = 0, 
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black')
+        pdf.multi_cell(w = 0, h = h_sep, txt = '2019/09/111', border = 0,#FECHA DE HOY FACTURA
+                 align = 'L', fill = 0)
+        
+        # fila 3 --
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = 'Apellido M:', border = 0, 
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black')
+        pdf.cell(w = 45, h = h_sep, txt = '31', border = 0,#AQUI VA NUMERO DE FACTURA
+                 align = 'L', fill = 0)
+        
+        tcol_set(pdf, 'gray')
+        pdf.cell(w = 45, h = h_sep, txt = '#FACTURA:', border = 0, #OTRO AQUI
+                 align = 'R', fill = 0)
+        
+        tcol_set(pdf, 'black')
+        pdf.multi_cell(w = 0, h = h_sep, txt = 'Retirado', border = 0,#AQUI VA
+                 align = 'L', fill = 0)
+        
+        
+        
+        
+        pdf.ln(15)
+        # tabla ----
+        
+        bcol_set(pdf, 'green')
+        tfont_size(pdf,15)
+        tfont(pdf,'B')
+        pdf.cell(w = 0, h = 15, txt = 'PRODUCTOS', border = 0,ln = 2, align = 'C', fill = 1)
+        tfont(pdf,'')
+        
+        tfont_size(pdf,13)
+        bcol_set(pdf, 'blue')
+        
+        pdf.cell(w = 20, h = 10, txt = 'ID', border = 0, align = 'C', fill = 1)
+        pdf.cell(w = 40, h = 10, txt = 'CODIGO DE BARRAS', border = 0, align = 'C', fill = 1)
+        pdf.cell(w = 70, h = 10, txt = 'CANTIDAD PRODUCTO', border = 0, align = 'C', fill = 1)
+        pdf.multi_cell(w = 0, h = 10, txt = 'PRECIO', border = 0, align = 'C',
+                     fill = 1)
+        
+        
+        tfont_size(pdf,12)
+        dcol_set(pdf, 'blue')
+        tcol_set(pdf, 'gray')
+        pdf.rect(x= 10, y= 60, w= 190, h= 53)
+        c = 0
+        for datos in lista_datos:
+        
+            c+=1
+            if(c%2==0):bcol_set(pdf, 'gray2')
+            else:bcol_set(pdf, 'white')
+        
+            pdf.cell(w = 20, h = 10, txt = str(datos[0]), border = 'TBL', align = 'C', fill = 1)
+            pdf.cell(w = 40, h = 10, txt = datos[1], border = 'TB', align = 'C', fill = 1)
+            pdf.cell(w = 70, h = 10, txt = datos[2], border = 'TB', align = 'C', fill = 1)
+            pdf.multi_cell(w = 0, h = 10, txt = datos[3], border = 'TBR', align = 'C', fill = 1)
+        
+        
+        
+        pdf.output('hoja.pdf')
+        
+        
+   
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                       
+        
+        
+        
+        
 
 
 
